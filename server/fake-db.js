75886ef314e8a470404d958e906b092728b6356d
jungdu <jungdujang@gmail.com>
2018-07-18 02:23:51 +0900

    adds booking creation functionality, adds improvements of rental routes

--- server/fake-db.js
+++ server/fake-db.js
@@ -41,6 +41,10 @@ class FakeDb{
             username: "Test User",
             email: "test@gmail.com",
             password: "testtest"
+        },{
+            username: "Test User1",
+            email: "test1@gmail.com",
+            password: "testtest1"
         }];
 
     }
@@ -51,14 +55,19 @@ class FakeDb{
     }
 
     pushRentalsToDb(){
+        const user = new User(this.users[0]);
+        const user2 = new User(this.users[1]);
+
         this.rentals.forEach((rental) => {
             const newRental = new Rental(rental);
+            newRental.user = user;
+
+            user.rentals.push(newRental);
             newRental.save();
         });
-        this.users.forEach((user) => {
-            const newUser = new User(user);
-            newUser.save();
-        });
+
+        user.save();
+        user2.save();
     }
     
     async seedDb(){
